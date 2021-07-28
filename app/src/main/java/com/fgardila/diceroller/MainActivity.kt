9b7f717f8f5344ccd22921f596dfa71a3ee53251
Fabian Guillermo Ardila Castro <40304898+fgardila@users.noreply.github.com>
2021-07-27 20:20:37 -0500

    Lesson 18: Exercise: Change the Text

--- app/src/main/java/com/fgardila/diceroller/MainActivity.kt
+++ app/src/main/java/com/fgardila/diceroller/MainActivity.kt
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.fgardila.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        binding.resultText.text = randomInt.toString()
     }
 }
