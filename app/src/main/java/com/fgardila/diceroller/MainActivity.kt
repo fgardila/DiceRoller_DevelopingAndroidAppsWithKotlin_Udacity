b2a4716640a28fdd846a0033de4990f2c300bc92
Fabian Guillermo Ardila Castro <40304898+fgardila@users.noreply.github.com>
2021-07-27 20:13:57 -0500

    Lesson 16: Exercise: findViewById

--- app/src/main/java/com/fgardila/diceroller/MainActivity.kt
+++ app/src/main/java/com/fgardila/diceroller/MainActivity.kt
@@ -2,10 +2,17 @@ package com.fgardila.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fgardila.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.rollButton.text = "Let's Roll"
+
     }
 }
